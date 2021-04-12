66036fedff4051b678627f16b902ba13e7ecec63
Jessica <jessicakchell@gmail.com>
2021-04-12 16:42:41 +0200

    animate

--- js/main.js
+++ js/main.js
@@ -47,3 +47,21 @@ sr.reveal('.animate-bottom', {
 	distance: '25rem',
 	delay: 600,
 });
+
+// buttons
+const buttons = document.getElementById('btn-cta');
+buttons.forEach((btn) => {
+	btn.addEventListener('click', function (e) {
+		let x = e.clientX - e.target.offsetLeft;
+		let y = e.clientY - e.target.offsetTop;
+
+		let ripples = document.createElement('span');
+		ripples.style.left = x + 'px';
+		ripples.style.top = y + 'px';
+		btn.appendChild(ripples);
+
+		setTimeout(() => {
+			ripples.remove();
+		}, 1000);
+	});
+});
